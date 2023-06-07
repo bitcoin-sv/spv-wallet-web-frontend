9c0c0e3d095b8eb002d2294b45d29ee4d2966dfc
Wojciech Bińkowski <wojciech.binkowski@4chain.studio>
2023-06-07 08:25:38 +0200

    refactor(login): remove storing email and password in state, at this stage

--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -2,36 +2,14 @@ import { FormCard } from '@/components/FormCard/FormCard'
 import { Input } from '@/components/Input'
 import { ViewContent } from '@/components/_layout/ViewContent'
 import { Row } from '@/styles/grid'
-import { useState } from 'react'
 
 export const LoginPage = () => {
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-
-  // const emailIsValid = email.match(EMAIL_REGEX)
-
   return (
     <ViewContent centeredContent>
       <Row>
         <FormCard formType="login" headline="Login here" subheadline="and go to the wallet!" formLegend="Login form">
-          <Input
-            id="email"
-            type="email"
-            labelText="Email"
-            required
-            onChange={(event) => setEmail(event.target.value)}
-            value={email}
-          />
-          <Input
-            id="password"
-            type="password"
-            labelText="Password"
-            withIcon
-            togglePasswordVisibility
-            required
-            onChange={(event) => setPassword(event.target.value)}
-            value={password}
-          />
+          <Input id="email" type="email" labelText="Email" required />
+          <Input id="password" type="password" labelText="Password" withIcon togglePasswordVisibility required />
         </FormCard>
       </Row>
     </ViewContent>
