b148e69b1bbbe9214c74ededc08d922f64f0b812
Wojciech Bińkowski <wojciech.binkowski@4chain.studio>
2023-06-21 13:44:29 +0200

    feat(legal pages): implement back to main page/ dashboard button

--- src/views/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/views/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,10 +1,16 @@
 import { ViewContent } from '@/components/_layout/ViewContent'
 import { Column, Row } from '@/styles/grid'
 import { PrivacyPolicyContent } from '@/components/LegalContent/_content/'
+import { BackToDashboardButton } from '@/components/BackToDashboardButton/BackToDashboardButton'
 
 export const PrivacyPolicy = () => {
   return (
     <ViewContent>
+      <Row>
+        <Column>
+          <BackToDashboardButton />
+        </Column>
+      </Row>
       <Row>
         <Column>
           <PrivacyPolicyContent />
--- src/views/TermsAndConditions/TermsAndConditions.tsx
+++ src/views/TermsAndConditions/TermsAndConditions.tsx
@@ -1,10 +1,16 @@
 import { ViewContent } from '@/components/_layout/ViewContent'
 import { Column, Row } from '@/styles/grid'
 import { TermsAndConditionsContent } from '@/components/LegalContent'
+import { BackToDashboardButton } from '@/components/BackToDashboardButton/BackToDashboardButton'
 
 export const TermsAndConditions = () => {
   return (
     <ViewContent>
+      <Row>
+        <Column>
+          <BackToDashboardButton />
+        </Column>
+      </Row>
       <Row>
         <Column>
           <TermsAndConditionsContent />
