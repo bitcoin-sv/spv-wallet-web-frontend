fa8c3f4c3023825ac3cd79eab597eb8b1d373db3
WojtekBinkowski <124057036+WojtekBinkowski@users.noreply.github.com>
2023-06-22 13:56:04 +0200

    refactor(api): remove api token (#17)

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,4 @@ import axios from 'axios'
 
 export const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-  },
 })
