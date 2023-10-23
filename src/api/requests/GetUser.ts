844c8a67ec341333bc7a1321668b3d53f2089649
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-10-23 13:12:59 +0200

    fix(BUX-288): prevent caching responses when loading data from api

--- src/api/requests/GetUser.ts
+++ src/api/requests/GetUser.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
 export const getUser = async (apiUrl: string) => {
-  const { data: response } = await axios.get(`${apiUrl}/user`, { withCredentials: true })
+  const { data: response } = await axios.get(`${apiUrl}/user`, {
+    withCredentials: true,
+    headers: {
+      'Cache-Control': 'no-store, no-cache',
+    },
+  })
   return response
 }
