3b24c433f5b5674d49e0d7438b44a3cdf00d6159
NH <nazarii.hrohol@4chain.studio>
2023-11-23 10:28:05 +0200

    Converted SetupWebsocket to react hook

---
+++ src/hooks/useWebsocket/useWebsocket.ts
@@ -0,0 +1,67 @@
+import { useWsUrl } from '@/api/wsUrl.ts'
+import { useEffect, useState } from 'react'
+import { Centrifuge, MessageContext } from 'centrifuge'
+import { WebsocketTransaction } from '@/api/types/transaction.ts'
+import { toast } from 'react-toastify'
+
+export const useWebsocket = () => {
+  const [isInitialized, setIsInitialized] = useState(false)
+  const [retries, setRetries] = useState(0)
+  const wsUrl = useWsUrl()
+
+  const centrifuge = new Centrifuge(wsUrl, { websocket: WebSocket })
+
+  const connect = () => {
+    if (!isInitialized) {
+      centrifuge.connect()
+      setIsInitialized(true)
+    } else {
+      throw new Error('Websocket connection is already initialized')
+    }
+  }
+
+  const disconnect = () => {
+    centrifuge.disconnect()
+    setIsInitialized(false)
+  }
+
+  centrifuge.on('message', function (ctx: MessageContext) {
+    const event: WebsocketTransaction | undefined = ctx?.data
+
+    if (event !== null && typeof event == 'object' && !Array.isArray(event)) {
+      const { eventType, status, error } = event
+      switch (eventType) {
+        case 'create_transaction':
+          if (status === 'success') {
+            toast.success('Transaction successfully sent')
+          } else if (status === 'error') {
+            toast.error(error || 'Error while sending transaction')
+          }
+          break
+      }
+    }
+  })
+
+  centrifuge.on('error', function () {
+    setRetries((prevState) => prevState + 1)
+    if (retries >= 3) {
+      centrifuge.disconnect()
+    }
+  })
+
+  useEffect(() => {
+    const channel = 'bux-wallet'
+    const sub = centrifuge.newSubscription(channel)
+    sub.subscribe()
+
+    return () => {
+      sub.unsubscribe()
+      sub.removeAllListeners()
+    }
+  }, [])
+
+  return {
+    connect,
+    disconnect,
+  }
+}
