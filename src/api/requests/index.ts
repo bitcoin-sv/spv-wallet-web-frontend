8eb6e766957a5a45112048afdf4dc45457d86253
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-03-13 12:47:59 +0100

    refactor: adjust after rebasing

--- src/api/requests/index.ts
+++ src/api/requests/index.ts
@@ -1,3 +1,3 @@
 export * from './transaction'
 export * from './user'
-export * from './GetServerConfig'
+export * from './serverConfig'
--- src/api/types/index.ts
+++ src/api/types/index.ts
@@ -1,4 +1,4 @@
 export * from './user'
-export * from './public_config'
+export * from './serverConfig'
 export * from './pagination'
 export * from './transaction'
