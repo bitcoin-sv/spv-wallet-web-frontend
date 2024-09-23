346ac89aa218e9518caf8b20ded6b6b176b770d8
Sebastian Wierzbicki <swierzbicki@users.noreply.github.com>
2024-09-23 13:25:17 +0200

    fix(SPV-1064): error when user clicks on paymail input (#97)

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,12 +4,13 @@ import { InputStyled, InputWrapper, LabelStyled } from './Input.styles';
 import { InputProps } from './types';
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ labelText, className, id, customPlaceholder, inputOnLightBackground, children, rootProps, ...rest }, ref) => {
+  ({ labelText, className, id, customPlaceholder, inputOnLightBackground, children, rootProps, inputProps, ...rest }, ref) => {
     return (
       <InputWrapper className={className} {...rootProps}>
         <InputStyled
           {...rest}
           ref={ref}
+          {...inputProps}
           placeholder={customPlaceholder || labelText}
           inputOnLightBackground={inputOnLightBackground || undefined}
         />
