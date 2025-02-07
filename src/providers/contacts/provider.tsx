949c869378096df4a640fa2839db8b623628be84
Damian Żółtowski <damian.zoltowski@4chain.studio>
2025-02-07 10:04:02 +0100

    feat(SPV-1420): make totp work (#132)

--- src/providers/contacts/provider.tsx
+++ src/providers/contacts/provider.tsx
@@ -1,7 +1,6 @@
 import { searchContacts } from '@/api/requests/contact';
 import { Contact } from '@/api/types/contact';
 import { PaginationParams } from '@/api/types';
-import { usePikeContactsEnabled } from '@/hooks/useFeatureFlags';
 import { FC, PropsWithChildren, createContext, useCallback, useEffect, useMemo, useState } from 'react';
 import { useAuthorization } from '../authorization';
 import { errorMessage } from '@/utils/errorMessage';
@@ -19,11 +18,9 @@ export const ContactsProvider: FC<PropsWithChildren> = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [contacts, setContacts] = useState<Contact[] | null>(null);
-  const pikeContactsEnabled = usePikeContactsEnabled();
   const { authorization } = useAuthorization();
-
   const load = useCallback(async () => {
-    if (!pikeContactsEnabled || !authorization) {
+    if (!authorization) {
       return;
     }
     setLoading(true);
@@ -42,7 +39,7 @@ export const ContactsProvider: FC<PropsWithChildren> = ({ children }) => {
     } finally {
       setLoading(false);
     }
-  }, [authorization, pikeContactsEnabled]);
+  }, [authorization]);
 
   const refresh = useCallback(() => {
     load();
